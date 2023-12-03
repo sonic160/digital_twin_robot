bc0a8948d978931846d407b0952f7a85a96af613
Mc Court Killian <killian.mccourt@student-cs.fr>
2023-12-03 23:21:44 +0100

    updated  to train test val, added regularisation and shuffling to dataset

--- ik_model/rain_predict_lstm.m
+++ ik_model/rain_predict_lstm.m
@@ -1,11 +1,11 @@
 % Step 1: Generate the dataset
 num_time_series = 6;
-time_series_length = 6000;
-num_classes = 7;
+time_series_length = 1000;
+num_classes = 4;
 
 % Parameters
-numSeq = 2800;      % Number of sequences
-numFeatures = 7;   % Number of features
+numSeq = 20;      % Number of sequences
+numFeatures = 4;   % Number of features
 maxSeqLength = 1000; % Maximum sequence length
 
 % Generate random sequences
@@ -19,15 +19,21 @@ YTrain = categorical(randi(6, numSeq, 1)); % Random labels from 1 to 6
 %     end
 %     %}
 
-numClasses = 7;  % Number of classes
+numClasses = 4;  % Number of classes
 
 % Generate categorical sequence
 pattern = mod(0:numSeq-1, numClasses);
 categoricalSequence = categorical(pattern, 0:numClasses-1);
 disp(categoricalSequence);
+totalElements = numel(categoricalSequence);
+indexToKeep = round(0.8 * totalElements);
 
-XTrain = cellArray
-YTrain = categoricalSequence
+totalCells = numel(cellArray);
+index = round(0.8 * totalCells);
+XTrain = cellArray(1:index);
+XVal = cellArray(index:totalCells);
+YTrain = categoricalSequence(1:indexToKeep);
+YVal =categoricalSequence(indexToKeep:totalElements);
 figure
 plot(XTrain{1}')
 xlabel("Time Step")
@@ -36,12 +42,12 @@ title("Training Observation 1")
 %legend("Feature " + string(1:numFeatures),Location="northeastoutside")
 % Display the generated data
 
-miniBatchSize = 32;
+miniBatchSize = 128;
 % Step 2: Define the neural network
 
 inputSize = 6;
 numHiddenUnits = 100;
-numClasses = 7;
+numClasses = 4;
 
 layers = [
     sequenceInputLayer(inputSize)
@@ -56,12 +62,16 @@ options = trainingOptions("adam", ...
     GradientThreshold=1, ...
     MaxEpochs=50, ...
     MiniBatchSize=miniBatchSize, ...
+    ValidationData={XVal,YVal}, ... %new
+    ValidationFrequency=20, ...     %new
     SequenceLength="longest", ...
-    Shuffle="never", ...
+    L2Regularization = 0.0001, ...  %new
+    Shuffle="once", ...
     Verbose=0, ...
     Plots="training-progress");
 
 
 net = trainNetwork(XTrain,YTrain,layers,options);
+
 
 
