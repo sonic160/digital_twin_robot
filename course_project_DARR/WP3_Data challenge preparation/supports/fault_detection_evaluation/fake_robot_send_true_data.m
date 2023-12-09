4a83bab7cdf533e700df4384b7c56384f3c992de
sonic160 <zhiguo.zeng@centralesupelec.fr>
2023-12-09 22:06:12 +0100

    Create a demo of using GUI to load fault detection model and use fake robot to send testing data.

--- course_project_DARR/WP3_Data challenge preparation/supports/fault_detection_evaluation/fake_robot_send_true_data.m
+++ course_project_DARR/WP3_Data challenge preparation/supports/fault_detection_evaluation/fake_robot_send_true_data.m
@@ -8,18 +8,26 @@ clear; clc; rosshutdown;
 rosinit('localhost'); % Use the appropriate ROS master URI if needed
 
 % Call the condition_monitoring function
-condition_monitoring();
+path = '../data_collection/collected_data/task_fault/';
+filenames = {'data_motor_1.csv', 'data_motor_2.csv', 'data_motor_3.csv', 'data_motor_4.csv', 'data_motor_5.csv', 'data_motor_6.csv'};
+
+condition_monitoring(path, filenames);
 
 
-function condition_monitoring()
+function condition_monitoring(path, filenames)
+% This function reads the data in [path filenames] and sends them through
+% the conditino_monitoring topic.
+% The data are sent recursively: If a seqeunce is finished, it will restart
+% from the beginning.
+% When a new sequence starts, a msg of "Prepare to receive the data!" will
+% be sent first.
+
     pub = rospublisher('/condition_monitoring', 'std_msgs/String');
     msg = rosmessage(pub);
 
     rate = rosrate(10); % 10 Hz
 
     % Read the true data in the csv files.
-    path = '../data_collection/collected_data/task_fault/';
-    filenames = {'data_motor_1.csv', 'data_motor_2.csv', 'data_motor_3.csv', 'data_motor_4.csv', 'data_motor_5.csv', 'data_motor_6.csv'};
     cm_data = cell(1, 6);
     for i = 1:6
         tmp = readtable([path filenames{i}]);
@@ -40,21 +48,29 @@ function condition_monitoring()
         voltage(:, i) = tmp.voltage;
     end
 
-    idx = 1;
-    while(idx<=n_s)
-        % Get the bus servo status
-        pos = position(idx, :);
-        temp = temperature(idx, :);
-        vol = voltage(idx, :);
+    % Sending the testing sequence recursively.
+    while (1)
+    idx = 0;
+        while(idx<=n_s)
+            if idx == 0 % Send a starting signal.
+                % Create the message
+                msg.Data = 'Prepare to receive the data!';
+            else
+                % Get the bus servo status
+                pos = position(idx, :);
+                temp = temperature(idx, :);
+                vol = voltage(idx, :);
+        
+                % Create the message
+                msg.Data = sprintf('position: %s\n temperature: %s\n voltage: %s\n \n', ...
+                    mat2str(pos), mat2str(temp), mat2str(vol));
+            end
 
-        % Create the message
-        msg.Data = sprintf('position: %s\n temperature: %s\n voltage: %s\n \n', ...
-            mat2str(pos), mat2str(temp), mat2str(vol));
-
-        % Publish the message
-        send(pub, msg);
-
-        idx = idx + 1;        
-        waitfor(rate);
+            % Publish the message
+            send(pub, msg);
+    
+            idx = idx + 1;        
+            waitfor(rate);
+        end
     end
 end
--- course_project_DARR/WP3_Data challenge preparation/supports/fault_detection_evaluation/fault_detection.m
+++
@@ -1,5 +0,0 @@
-% This function takes the feature x and detect if we have failure
-% related to motor_id.
-function pred_state = fault_detection(feature)
-    pred_state = 1;
-end
