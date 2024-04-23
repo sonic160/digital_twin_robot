5233b219f999596b50ef1968f50d241a24afa7d3
Pierre Le Goff <pierre.lgfs@student-cs.fr>
2024-04-23 11:36:59 +0200

    pierre notebook

--- robot_digital_twin/condition_monitoring_matlab_ros/generate_msg.m
+++ robot_digital_twin/condition_monitoring_matlab_ros/generate_msg.m
@@ -1,5 +1,5 @@
 clear; clc;
 
-base_dictionary = 'Z:'
-folderpath = 'Z:\cm\';
+base_dictionary = '/Users/user1/Desktop/Industry_40/digital_twin_robot_group2/robot_digital_twin/catkin_ws'
+folderpath = '/Users/user1/Desktop/Industry_40/digital_twin_robot_group2/robot_digital_twin/catkin_ws/cm';
 rosgenmsg(base_dictionary)
