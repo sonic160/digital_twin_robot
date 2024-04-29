ffdd9c5231855abf024880ee69905ef1998fa257
MaxDefez <max.defez@student-cs.fr>
2024-04-29 10:53:04 +0200

    pull

--- robot_digital_twin/condition_monitoring_matlab_ros/generate_msg.m
+++ robot_digital_twin/condition_monitoring_matlab_ros/generate_msg.m
@@ -1,6 +1,6 @@
 clear; 
 clc;
 
-base_dictionary = 'C:\Users\maxde\Documents\CS\2A\4.0\digital_twin_robot_group2\robot_digital_twin\catkin_ws';
-folderpath = 'C:\Users\maxde\Documents\CS\2A\4.0\digital_twin_robot_group2\robot_digital_twin\catkin_ws\cm';
+base_dictionary = 'C:\Users\maxde\Documents\CS\2A\4_0\digital_twin_robot_group2\robot_digital_twin\catkin_ws';
+folderpath = 'C:\Users\maxde\Documents\CS\2A\4_0\digital_twin_robot_group2\robot_digital_twin\catkin_ws\cm';
 rosgenmsg(base_dictionary)
