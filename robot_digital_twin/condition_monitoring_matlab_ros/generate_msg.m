5c746f7cd479e8c874edea20f6f8e5a2eca397a0
Zhoulong0 <lon.zhou@stud.uniroma3.it>
2024-06-04 10:53:41 +0200

    new data

--- robot_digital_twin/condition_monitoring_matlab_ros/generate_msg.m
+++ robot_digital_twin/condition_monitoring_matlab_ros/generate_msg.m
@@ -1,5 +1,5 @@
 clear; clc;
 
-base_dictionary = 'Z:'
-folderpath = 'Z:\cm\';
+base_dictionary = '/Users/longzhou/Library/CloudStorage/OneDrive-PolitecnicodiMilano/group_7/robot_digital_twin/catkin_ws'
+folderpath = '/Users/longzhou/Library/CloudStorage/OneDrive-PolitecnicodiMilano/group_7/robot_digital_twin/catkin_ws/cm'
 rosgenmsg(base_dictionary)
